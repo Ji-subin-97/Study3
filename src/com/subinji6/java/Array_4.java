7b3e52a6e7733bfac5f2fb6141ad8740fcf00012
Ji-subin-97 <qls1178@naver.com>
2021-07-15 17:49:33 +0900

    Update Array_4.java

--- src/com/subinji6/java/Array_4.java
+++ src/com/subinji6/java/Array_4.java
@@ -3,18 +3,18 @@ package com.subinji6.java;
 public class Array_4 {
 
 	public static void main(String[] args) {
-		int [] ar1 = new int [3];
-		ar1[0] = 1;
-		ar1[1] = 2;
-		ar1[2] = 3;
+		int [] ar1 = new int [5];
+		for(int i=0;i<ar1.length;i++) {
+			ar1[i]=i+1;
+		}
 		
 		int [] ar2 = new int[ar1.length+1];
 		
 		for(int i=0; i<ar1.length;i++) {
 			ar2[i] =ar1[i];
+			ar2[ar1.length] = ar2.length;
 		}
 		
-		ar2[ar1.length] = ar2.length;
 		ar1 = ar2;
 
 		
